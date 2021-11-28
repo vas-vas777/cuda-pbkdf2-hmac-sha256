226fcc1c2beaabd0b6fd3f864ae3301dea34e8df
vas-vas777 <vasya1998@list.ru>
2021-11-28 10:35:31 +0300

    Add files via upload

---
+++ kernel.cu
@@ -0,0 +1,190 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+#include <string>
+#include <iostream>
+#include <bitset>
+#include<thrust/copy.h>
+
+
+
+//cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+//__global__ void addKernel(int *c, const int *a, const int *b)
+//{
+//    int i = threadIdx.x;
+//    c[i] = a[i] + b[i];
+//}
+
+int main()
+{
+    /*const int arraySize = 5;
+    const int a[arraySize] = { 1, 2, 3, 4, 5 };
+    const int b[arraySize] = { 10, 20, 30, 40, 50 };
+    int c[arraySize] = { 0 };*/
+    thrust::host_vector<char> hash_output;
+    //cudaError_t cudaStatus;
+    std::string password = "1234";
+    std::string binary_str;
+    for (size_t i = 0; i < password.size(); ++i)
+    {
+        binary_str.append(std::bitset<8>(password.c_str()[i]).to_string());
+    }
+
+    thrust::host_vector<char> vec_binary_str;
+    for (auto i = 0; i < binary_str.size(); ++i)
+    {
+        vec_binary_str.push_back(binary_str[i]);
+    }
+
+    thrust::device_vector<char> binary_passwd = vec_binary_str;
+
+    thrust::device_vector<char>pass = binary_passwd;
+    pass.push_back('1'); //add 1 
+    for (auto i = 0; i < 447 - pass.size(); ++i) // add '0' multiple 512 
+    {
+        pass.push_back('0');
+    }
+    thrust::device_vector<char>length_pass_in_binary;
+    char bit;
+    size_t number_of_bits_password = binary_passwd.size();
+    while (number_of_bits_password != 0)
+    {
+        bit = number_of_bits_password % 2 ? 1 : 0;
+        length_pass_in_binary.push_back(bit);
+        number_of_bits_password /= 2;
+    }
+    for (auto i = 0; i < 63 - length_pass_in_binary.size(); ++i)
+    {
+        pass.push_back('0');
+    }
+    for (auto i = length_pass_in_binary.size() - 1; i > 0; --i)
+    {
+        pass.push_back(length_pass_in_binary[i]);
+    }
+    hash_output = pass;
+
+    //addWithCuda(binary_passwd, binary_passwd.size(), hash_output);
+       /* if (cudaStatus != cudaSuccess) {
+            fprintf(stderr, "addWithCuda failed!");
+            return 1;
+        }*/
+        // }
+    for (auto i : hash_output)
+    {
+        std::cout << i;
+
+    }
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    /*cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }*/
+
+    // Add vectors in parallel.
+   /* cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addWithCuda failed!");
+        return 1;
+    }
+
+    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
+        c[0], c[1], c[2], c[3], c[4]);*/
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+   /* cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }*/
+
+    return 0;
+}
+
+// Helper function for using CUDA to add vectors in parallel.
+//cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
+//{
+//    int *dev_a = 0;
+//    int *dev_b = 0;
+//    int *dev_c = 0;
+//    cudaError_t cudaStatus;
+//
+//    // Choose which GPU to run on, change this on a multi-GPU system.
+//    cudaStatus = cudaSetDevice(0);
+//    if (cudaStatus != cudaSuccess) {
+//        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+//        goto Error;
+//    }
+//
+//    // Allocate GPU buffers for three vectors (two input, one output)    .
+//    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
+//    if (cudaStatus != cudaSuccess) {
+//        fprintf(stderr, "cudaMalloc failed!");
+//        goto Error;
+//    }
+//
+//    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
+//    if (cudaStatus != cudaSuccess) {
+//        fprintf(stderr, "cudaMalloc failed!");
+//        goto Error;
+//    }
+//
+//    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
+//    if (cudaStatus != cudaSuccess) {
+//        fprintf(stderr, "cudaMalloc failed!");
+//        goto Error;
+//    }
+//
+//    // Copy input vectors from host memory to GPU buffers.
+//    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
+//    if (cudaStatus != cudaSuccess) {
+//        fprintf(stderr, "cudaMemcpy failed!");
+//        goto Error;
+//    }
+//
+//    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
+//    if (cudaStatus != cudaSuccess) {
+//        fprintf(stderr, "cudaMemcpy failed!");
+//        goto Error;
+//    }
+//
+//    // Launch a kernel on the GPU with one thread for each element.
+//    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
+//
+//    // Check for any errors launching the kernel
+//    cudaStatus = cudaGetLastError();
+//    if (cudaStatus != cudaSuccess) {
+//        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+//        goto Error;
+//    }
+//    
+//    // cudaDeviceSynchronize waits for the kernel to finish, and returns
+//    // any errors encountered during the launch.
+//    cudaStatus = cudaDeviceSynchronize();
+//    if (cudaStatus != cudaSuccess) {
+//        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+//        goto Error;
+//    }
+//
+//    // Copy output vector from GPU buffer to host memory.
+//    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+//    if (cudaStatus != cudaSuccess) {
+//        fprintf(stderr, "cudaMemcpy failed!");
+//        goto Error;
+//    }
+//
+//Error:
+//    cudaFree(dev_c);
+//    cudaFree(dev_a);
+//    cudaFree(dev_b);
+//    
+//    return cudaStatus;
+//}
