ebd24a032c08dc8d0e9bf59d9f86602b9ba86280
vas-vas777 <vasya1998@list.ru>
2021-11-30 21:24:41 +0300

    Add files via upload

--- kernel.cu
+++ kernel.cu
@@ -11,11 +11,7 @@
 #include <thrust/copy.h>
 
 
-cudaError_t addWithCuda(char* binary_pass, int length_block);
-
-__global__ void addKernel(char* binary_pass, int length_block)
-{
-    /*auto h0 = 0x6a09e667;
+/*auto h0 = 0x6a09e667;
     auto  h1 = 0xbb67ae85;
     auto  h2 = 0x3c6ef372;
     auto  h3 = 0xa54ff53a;
@@ -34,109 +30,274 @@ __global__ void addKernel(char* binary_pass, int length_block)
 
     int index = threadIdx.x;
     int w[64] = {};*/
+
+cudaError_t addWithCuda(char* binary_pass, int length_block);
+
+__host__ __int64* password_xor_with_IPAD(char* password,int length)
+{
+    __int64* binary_str = new __int64[512]{ 0 };
+    __int64 IPAD[] = { 0,0,1,1,0,1,1,0 };
+    __int64* ipad = new __int64[512]{ 0 };
+    for (auto i = 0; i < 512; ++i)
+    {
+        if (i < 8 * length)
+        {
+            binary_str[i] = (0 != (password[i / 8] & 1 << (~i & 7)));
+        }
+        else
+        {
+            binary_str[i] = 0;
+        }
+    }
+    for (auto i = 0; i < 512; ++i)
+    {
+        ipad[i] = IPAD[i % 8];
+        
+    }
+   /* for (auto i = 0; i < 512; ++i)
+    {
+        std::cout << binary_str[i];
+    }
+    std::cout << std::endl;
+    for (auto i = 0; i < 512; ++i)
+    {
+        std::cout << ipad[i];
+    }
+    std::cout << std::endl;*/
+    for (auto i = 0; i < 512; ++i)
+    {
+        binary_str[i] = binary_str[i] ^ ipad[i];
+    }
+    for (auto i = 0; i < 512; ++i)
+    {
+        std::cout << binary_str[i];
+    }
+    std::cout << std::endl;
+    std::cout << sizeof(binary_str) << std::endl;
+    return binary_str;
+    //std::string binary_str = {};
+    //std::string IPAD = {};
+    ////std::cout << binary_pass << std::endl;
+    //for (auto i = 0; i < 64; ++i) //перевод в двоичную строку пароля
+    //{
+    //    if (i < password.size())
+    //    {
+    //        binary_str.append(std::bitset<8>(password.c_str()[i]).to_string());
+    //        IPAD.append(std::bitset<8>('0x36').to_string());
+    //    }
+    //    else
+    //    {
+    //        binary_str.append(std::bitset<8>(0).to_string());
+    //        IPAD.append(std::bitset<8>('0x36').to_string());
+    //    }
+    //}
+    /////std::cout << binary_str << std::endl;
+    ////std::cout << std::endl;
+    ////std::cout << IPAD << std::endl;
+    ////std::cout << binary_str.size() << " " << IPAD.size() << std::endl;
+    //binary_str = (std::bitset<512>(binary_str) ^ std::bitset<512>(IPAD)).to_string();
+    //return binary_str;
+    //std::cout << binary_str << std::endl;
+    delete[] binary_str;
+    delete[] ipad;
+}
+
+__device__ int* password_xor_with_OPAD(char* password, int length)
+{
+    int* binary_str = new int[512]{ 0 };
+    int OPAD [] = { 0,1,0,1,1,1,0,0 };
+    int* opad = new int[512]{ 0 };
+    for (auto i = 0; i < 512; ++i)
+    {
+        if (i < 8 * length)
+        {
+            binary_str[i] = (0 != (password[i / 8] & 1 << (~i & 7)));
+        }
+        else
+        {
+            binary_str[i] = 0;
+        }
+    }
+    for (auto i = 0; i < 512; ++i)
+    {
+        opad[i] = OPAD[i % 8];
+
+    }
+    /*for (auto i = 0; i < 512; ++i)
+    {
+        std::cout << binary_str[i];
+    }
+    std::cout << std::endl;
+    for (auto i = 0; i < 512; ++i)
+    {
+        std::cout << opad[i];
+    }
+    std::cout << std::endl;*/
+    for (auto i = 0; i < 512; ++i)
+    {
+        binary_str[i] = binary_str[i] ^ opad[i];
+    }
+   /* for (auto i = 0; i < 512; ++i)
+    {
+        std::cout << binary_str[i];
+    }*/
+
+
+    return binary_str;
+    delete[] binary_str;
+    delete[] opad;
+    //std::string binary_str = {};
+    //std::string IPAD = {};
+    ////std::cout << binary_pass << std::endl;
+    //for (auto i = 0; i < 64; ++i) //перевод в двоичную строку пароля
+    //{
+    //    if (i < password.size())
+    //    {
+    //        binary_str.append(std::bitset<8>(password.c_str()[i]).to_string());
+    //        IPAD.append(std::bitset<8>('0x5C').to_string());
+    //    }
+    //    else
+    //    {
+    //        binary_str.append(std::bitset<8>(0).to_string());
+    //        IPAD.append(std::bitset<8>('0x5C').to_string());
+    //    }
+    //}
+    /////std::cout << binary_str << std::endl;
+    ////std::cout << std::endl;
+    ////std::cout << IPAD << std::endl;
+    ////std::cout << binary_str.size() << " " << IPAD.size() << std::endl;
+    //binary_str = (std::bitset<512>(binary_str) ^ std::bitset<512>(IPAD)).to_string();
+    //return binary_str;
+}
+
+ __host__ void preparation_sha256_with_IPAD(__int64* password_xor_with_ipad, __int64*prev_hash)
+{
+     __int64* binary_str = new __int64[1024]{ 0 };
+
+    //memcpy(binary_str, password_xor_with_ipad, 512);
+    std::cout << std::endl;
+    for (auto i = 0; i < 1024; ++i)
+    {
+        std::cout << binary_str[i];
+    }
+    std::cout << std::endl;
+    memcpy(binary_str, password_xor_with_ipad, 256);
+    //memmove(binary_str, password_xor_with_ipad, 512);
+    for (auto i = 0; i < 1024; ++i)
+    {
+        std::cout << binary_str[i];
+    }
+    std::cout << std::endl;
+   /* memmove(binary_str+256, prev_hash, 256);
+    for (auto i = 0; i < 1024; ++i)
+    {
+        std::cout << binary_str[i];
+    }*/
+
+    //std::string binary_str = password_xor_with_ipad;
+    //std::string binary_Ui = {};
+    //for (auto i = 0; i < Ui.size(); ++i)
+    //{
+    //    binary_Ui.append(std::bitset<8>(Ui.c_str()[i]).to_string());
+    //}
+    //binary_str = binary_str + binary_Ui;//512+256 bits=768bits
+    //int size_binary_str = binary_str.size();//length of input message (pass xor OPAD)||U(i-1))
+    //binary_str.append("1"); //add 1 bit =769 bit
+    //for (auto i = 0; i < 191; ++i) // add '0' multiple 512 => 769+191=960 mod 512 = 448 mod 512
+    //{
+    //    binary_str.append("0");
+    //}
+    //std::vector<int>length_pass_in_binary = {};//binary vector of size input message  
+    //int bit=0;
+    ////size_t number_of_bits_password = binary_.size();
+    //while (size_binary_str != 0)
+    //{
+    //    bit = size_binary_str % 2 ? 1 : 0;
+    //    length_pass_in_binary.push_back(bit);
+    //    size_binary_str /= 2;
+    //}
+    //for (auto i = 0; i < 64 - length_pass_in_binary.size(); ++i)//add last 64 bits, where 64 - length_pass_in_binary '0'
+    //{
+    //    binary_str.append("0");
+    //}
+    //for (auto i = 0; i < length_pass_in_binary.size(); ++i)//add binary vector of value input message
+    //{
+    //   binary_str.push_back((char)(length_pass_in_binary[length_pass_in_binary.size() - 1 - i]) + 48);
+    //}
+
+    //// std::cout << hash_output.size() << std::endl;
+    //// char* bin_str_512 = (char*)malloc(hash_output.size());
+    //for (auto i : binary_str)//output 1024 binary message
+    //{
+    //    std::cout << i;
+    //}
+    //std::cout << std::endl;
+    //char **w = new char*[64];
+    //for (auto i = 0; i < 64; ++i)
+    //    w[i] = new char[32];
+    //
+    //// thrust::copy(hash_output.begin(), hash_output.end(), &bin_str_512[0]);
+    ////std::string message_schedule[64];
+    ////for (auto i = 0; i < 64; ++i)
+    ////{
+    ////    if (i < 16)
+    ////    {
+    ////        for (auto j = 0; j < 32; ++j)
+    ////        {
+    ////            message_schedule[i] += pass[j + 32 * i];
+    ////        }
+    ////    }
+    ////    else
+    ////    {
+    ////        for (auto j = 0; j < 32; ++j)
+    ////        {
+    ////            message_schedule[i] += '0';
+    ////        }
+    ////    }
+    ////}
+    ////for (auto i : message_schedule)
+    ////{
+    ////    std::cout << i << std::endl;
+    ////}
+    ////std::cout << std::endl;
+    ////char message_schedule_d[64][32];
+    //int count=0;
+    //for (auto i = 0; i < 64; ++i)
+    //{
+    //    for (auto j = 0; j < 32; ++j)
+    //    {
+    //        //std::cout << i << " " << j << std::endl;
+    //        w[i][j] = binary_str[count];
+    //        count++;
+    //    }
+    //    count++;
+    //}
+    //for (auto i = 0; i < 64; ++i)
+    //{
+    //    for (auto j = 0; j < 32; ++j)
+    //    {
+    //        std::cout << w[i][j];
+    //    }
+    //    std::cout << std::endl;
+    //}
+    //return w;
+}
+
+__global__ void addKernel(char* binary_pass, int length_block)
+{
+    
     
 
 
 }
 int main()
 {
-   /* const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };*/
-   // std::cout << '0' + '1' << std::endl;
-    //cudaError_t cudaStatus;
+   
     std::string password = "1234";
-    std::string binary_str;
-    //char* binary_pass = "";
-    std::vector<char>hash_output;
-    //std::cout << binary_pass << std::endl;
-    for (size_t i = 0; i < password.size(); ++i) //перевод в двоичную строку пароля
-    {
-        binary_str.append(std::bitset<8>(password.c_str()[i]).to_string());
-    }
-    //binary_pass = &binary_str[0];
-    std::vector<char> binary_passwd;
-    for (auto i = 0; i < binary_str.size(); ++i)
-    {
-        binary_passwd.push_back(binary_str[i]);
-    }
-
-    //thrust::host_vector<char> binary_passwd = vec_binary_str;
-
-    std::vector<char>pass = binary_passwd;
-    pass.push_back('1'); //add 1 
-    size_t length_bin_pass = pass.size();
-    for (auto i = 0; i < 448 - length_bin_pass; ++i) // add '0' multiple 512 k=448 (mod512)
-    {                                               
-        pass.push_back('0');
-    }
-    std::vector<int>length_pass_in_binary;
-    int bit;
-    size_t number_of_bits_password = binary_passwd.size();
-    while (number_of_bits_password != 0)//перевод длины пароля в двоичную запись
-    {
-        bit = number_of_bits_password % 2 ? 1 : 0;
-        length_pass_in_binary.push_back(bit);
-        number_of_bits_password /= 2;
-    }
-    for (auto i = 0; i < 64 - length_pass_in_binary.size(); ++i)//добавляем нули 
-    {
-        pass.push_back('0');
-    }
-    for (auto i = 0; i <length_pass_in_binary.size(); ++i)//добовляем все в общую двоичную строку
-    {
-        pass.push_back((char)(length_pass_in_binary[length_pass_in_binary.size()-1-i])+48);
-    }
-    
-   // std::cout << hash_output.size() << std::endl;
-   // char* bin_str_512 = (char*)malloc(hash_output.size());
-    for (auto i : pass)
-    {
-        std::cout << i;
-    }
-    std::cout << std::endl;
-   // thrust::copy(hash_output.begin(), hash_output.end(), &bin_str_512[0]);
-    std::string message_schedule[64];//создаём массив 64 строк по 32 бита. первые 16 строк по 32 бита из pass. Остальные нули - 48 строк
-    for (auto i = 0; i < 64; ++i)
-    {
-        if (i < 16)
-        {
-            for (auto j = 0; j < 32; ++j)
-            {
-                message_schedule[i] += pass[j + 32 * i];
-            }
-        }
-        else
-        {
-            for (auto j = 0; j < 32; ++j)
-            {
-                message_schedule[i] += '0';
-            }
-        }
-    }
-    for (auto i : message_schedule)
-    {
-        std::cout << i << std::endl;
-    }
-    std::cout << std::endl;
-    char message_schedule_d[64][32]; //копируем из массива std::string message_schedule[64] char message_schedule_d[64][32] для дальнейших вычислений в cuda __global__ void addKerne
-    for (auto i = 0; i < 64; ++i)
-    {
-        for (auto j = 0; j < 32; ++j)
-        {
-            //std::cout << i << " " << j << std::endl;
-            message_schedule_d[i][j] = message_schedule[i][j];
-        }
-    }
-    for (auto i = 0; i < 64; ++i)
-    {
-        for (auto j = 0; j < 32; ++j)
-        {
-            std::cout << message_schedule_d[i][j];
-        }
-        std::cout << std::endl;
-    }
+    __int64* password_xor_with_ipad = password_xor_with_IPAD("1234", 4);
+    __int64* prev_hash_u = new __int64[256]{ 2 };
+    preparation_sha256_with_IPAD(password_xor_with_ipad, prev_hash_u);
+    //password_xor_with_IPAD("1234", 4);
     // Add vectors in parallel.
     /*cudaError_t cudaStatus = addWithCuda(bin_str_512, 512);
     if (cudaStatus != cudaSuccess) {
